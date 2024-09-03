7c70bffffa8e7002ce95f08f5b240f907a6e621d
mr.l <admin@duxphp.com>
2024-09-04 01:09:44 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import routes from './routes'
 import logger from './utils/logger'
-import path from 'path';
+import path from 'path'
+import cors from 'cors'
 
 
 const app = express()
+
+app.use(cors())
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.html', require('ejs').__express)
 app.set('view engine', 'html')
