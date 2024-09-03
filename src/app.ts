892316eccf1576f98479e06d2a2562836b86e748
mr.l <admin@duxphp.com>
2024-09-04 01:27:04 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.engine('.html', require('ejs').__express)
 app.set('view engine', 'html')
 app.use(express.json())
 
-const PORT = 1730
+const PORT = 3000
 
 // 启动
 app.listen(PORT, async () => {
